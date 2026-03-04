77cf853a437633071ac25de0ecdf29e12e5e1f69
Soumya Ranjan Nanda <222658249+SoumyaRanjanNanda@users.noreply.github.com>
2026-03-05 03:36:49 +0530

    skeleton code
    java skeleton code

---
+++ Introduction/Introduction.java
@@ -0,0 +1,11 @@
+// If a class is declared public, then the file name must be exactly the same as the class name, otherwise the compiler gives an error.
+public class introductio{
+    public static void main(Strimg [] args)
+
+  {
+
+System.out.println("Hello SoumyaRanjanNanda");
+
+  }
+  
+}
