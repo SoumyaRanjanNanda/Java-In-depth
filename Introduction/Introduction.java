dcf233e17c98f2df0317f2f07c6690049318bddd
Soumya Ranjan Nanda <222658249+SoumyaRanjanNanda@users.noreply.github.com>
2026-03-05 03:39:08 +0530

    Fix class name and main method declaration
    Correct class name from 'introductio' to 'introduction' and fix 'Strimg' to 'String'.

--- Introduction/Introduction.java
+++ Introduction/Introduction.java
@@ -1,6 +1,6 @@
 // If a class is declared public, then the file name must be exactly the same as the class name, otherwise the compiler gives an error.
-public class introductio{
-    public static void main(Strimg [] args)
+public class introduction{
+    public static void main(String [] args)
 
   {
 
